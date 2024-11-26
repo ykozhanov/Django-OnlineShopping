06a9116b238486cf7353a100506681299c463e78
tab <alexey.khaustov@yandex.ru>
2024-11-26 23:25:17 +0300

    Отображение для редактирования всех полей модели User

--- src/profiles/admin.py
+++ src/profiles/admin.py
@@ -2,27 +2,45 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
-from .models import User
+from django.contrib.auth import get_user_model
 
 
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
-    model = User
+    model = get_user_model()
     list_display = ('email', 'is_staff', 'is_active',)
     list_filter = ('email', 'is_staff', 'is_active',)
+    # fieldsets = (
+    #     (None, {'fields': ('email', 'password')}),
+    #     ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    # )
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+        (None, {"fields": ("email", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name",)}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_seller",
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active', "is_seller")}
         ),
     )
     search_fields = ('email',)
     ordering = ('email',)
 
 
-admin.site.register(User, CustomUserAdmin)
+admin.site.register(get_user_model(), CustomUserAdmin)
