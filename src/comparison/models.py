1c5843a101bedefea52892e11976b4e9d932cd9f
ykozhanov <ykozhanov97@gmail.com>
2024-12-31 10:28:04 +0500

    Add model ComparisonList

--- src/comparison/models.py
+++ src/comparison/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from products.models import Product
 
-# Create your models here.
+User = get_user_model()
+
+
+class ComparisonList(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product, related_name="comparisons")
+
+    def __str__(self):
+        return f"Comparison list for {self.user.email}"
