376b33df067fffc18beabda587651c83cfff63ba
ykozhanov <ykozhanov97@gmail.com>
2025-01-17 17:41:59 +0500

    Model -> Session

--- src/comparison/views.py
+++ src/comparison/views.py
@@ -1,47 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
-from django.http import JsonResponse
+from django.http import HttpRequest, JsonResponse
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import QuerySet
 
 from products.models import Product
-from .models import ComparisonList
 
 
 class ComparisonView(LoginRequiredMixin, View):
 
-    @staticmethod
-    def get_comparison_list(user):
-        comparison_list, created = ComparisonList.objects.get_or_create(user=user)
-        return comparison_list
-
-    def post(self, request, product_id):
-        product = Product.objects.get(id=product_id)
-        comparison_list = self.get_comparison_list(request.user)
-        comparison_list.add_product(product)
+    def post(self, request: HttpRequest, product_id: int) -> JsonResponse:
+        product = get_object_or_404(Product, id=product_id)
+        comparison_list: list[int] = request.session.get('comparison_list', [])
+        if product_id not in comparison_list:
+            comparison_list.append(product_id)
+        request.session['comparison_list'] = comparison_list
         return JsonResponse({'message': 'Товар добавлен в список сравнения'})
 
-    def delete(self, request, product_id):
-        product = Product.objects.get(id=product_id)
-        comparison_list = self.get_comparison_list(request.user)
-        comparison_list.remove_product(product)
+    def delete(self, request: HttpRequest, product_id: int) -> JsonResponse:
+        comparison_list: list[int] = request.session.get('comparison_list', [])
+        if product_id in comparison_list:
+            index = comparison_list.index(product_id)
+            comparison_list.pop(index)
+        request.session['comparison_list'] = comparison_list
         return JsonResponse({'message': 'Товар убран из списка сравнения'})
 
-    def get(self, request):
+    def get(self, request) -> JsonResponse:
         limit = int(request.GET.get("limit", "3"))
-        comparison_list = self.get_comparison_list(request.user)
+        comparison_list: list[int] = request.session.get('comparison_list', [])
 
-        context = self.compare_products(comparison_list, limit)
-        return render(request, template_name="comparison/comparison.html", context=context)
+        context = self._compare_products(comparison_list, limit)
+        return render(request, template_name='comparison/comparison.html', context=context)
 
     @staticmethod
-    def compare_products(comparison_list, limit):
-        products = comparison_list.get_products(limit)
-
+    def _compare_products(comparison_list: list[int], limit: int):
+        products: QuerySet[Product] = Product.objects.filter(id__in=comparison_list)[:limit]
         if len(products) < 2:
             return {
-                "message": 'Недостаточно товаров для сравнения',
-                "products": products,
-                "comparison_data": {}
+                'message': 'Недостаточно товаров для сравнения',
+                'products': products,
+                'comparison_data': {},
             }
 
         common_characteristics = set(products[0].characteristic_values.keys())
