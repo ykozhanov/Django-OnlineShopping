67636f09e1c756169b381a7a1ef2087bea2715bb
Yakovlev Sergey <monsterroot@gmail.com>
2025-01-20 01:41:18 +1000

    add annotations in sellers admin

--- src/sellers/admin.py
+++ src/sellers/admin.py
@@ -1,8 +1,13 @@
+from typing import Optional
+
 from django import forms
 from django.contrib import admin
 from django.contrib.admin.actions import delete_selected
 from django.contrib.auth import get_user_model
-from django.utils.safestring import mark_safe
+from django.contrib.auth.models import User
+from django.db.models import QuerySet
+from django.http import HttpRequest
+from django.utils.safestring import mark_safe, SafeString
 
 from products.models import Category, Product
 from .models import Seller, ProductSeller
@@ -10,7 +15,7 @@ from .models import Seller, ProductSeller
 User = get_user_model()
 
 
-def has_moderators_groups(user):
+def has_moderators_groups(user: User) -> bool:
     return user.is_superuser or user.groups.filter(name="moderators").exists()
 
 
@@ -24,22 +29,22 @@ class SellerAdmin(admin.ModelAdmin):
     readonly_fields = ("get_image",)
     actions = None
 
-    def get_fields(self, request, obj=None):
+    def get_fields(self, request: HttpRequest, obj=None)-> QuerySet:
         """Удаляет поле владелец товара если не moderators"""
         if not has_moderators_groups(request.user):
             if "user" in self.fields:
                 self.fields.remove("user")
         return self.fields
 
-    def has_delete_permission(self, request, obj=None):
+    def has_delete_permission(self, request: HttpRequest, obj=None)-> bool:
         """Удаляет кнопку delete из формы если не moderators"""
         return has_moderators_groups(request.user)
 
-    def has_add_permission(self, request):
+    def has_add_permission(self, request: HttpRequest)-> bool:
         """Позволяет moderators добавлять seller"""
         return has_moderators_groups(request.user)
 
-    def get_actions(self, request):
+    def get_actions(self, request: HttpRequest) -> dict:
         """Добавляет delete_selected если moderators"""
         actions = super().get_actions(request)
         if has_moderators_groups(request.user):
@@ -50,7 +55,7 @@ class SellerAdmin(admin.ModelAdmin):
             )
         return actions
 
-    def get_queryset(self, request):
+    def get_queryset(self, request: HttpRequest)-> QuerySet:
         """Возвращает seller для отображения. У модератора возвращает список всех seller."""
         queryset = super().get_queryset(request)
         if not has_moderators_groups(request.user):
@@ -58,7 +63,7 @@ class SellerAdmin(admin.ModelAdmin):
         return queryset
 
     @admin.display(description="Изображение")
-    def get_image(self, seller: Seller):
+    def get_image(self, seller: Seller)-> Optional[SafeString]:
         """Возвращает изображение seller если оно есть."""
         if seller.image:
             return mark_safe(f'<img src="{seller.image.url}" width="50" height="50">')
@@ -74,7 +79,7 @@ class ProductSellerForm(forms.ModelForm):
         self.user = getattr(self, "current_user", None)
         self.get_products_for_category()
 
-    def get_products_for_category(self):
+    def get_products_for_category(self)-> None:
         """возвращает products по установленной category при создании формы или ошибке валидации"""
         category_id = self.data.get("category")
         if category_id:
@@ -98,20 +103,20 @@ class ProductSellerAdmin(admin.ModelAdmin):
     search_fields = ("product__name", "seller__name")
     list_per_page = 20
 
-    def get_list_display(self, request):
+    def get_list_display(self, request: HttpRequest)-> list:
         """удаляет поле отображения seller если не moderators"""
         list_display = [*self.list_display]
         if not has_moderators_groups(request.user) and "seller" in list_display:
             list_display.remove("seller")
         return list_display
 
-    def get_queryset(self, request):
+    def get_queryset(self, request: HttpRequest)-> QuerySet:
         """Возвращает список моделей product-seller для отображения."""
         if has_moderators_groups(request.user):
             return ProductSeller.objects.all()
         return ProductSeller.objects.filter(seller__user=request.user)
 
-    def get_form(self, request, obj=None, **kwargs):
+    def get_form(self, request: HttpRequest, obj=None, **kwargs)-> ProductSellerForm:
         """передаёт текущего user в форму добавления товара"""
         form = super().get_form(request, obj, **kwargs)
         if not has_moderators_groups(request.user):
@@ -120,12 +125,12 @@ class ProductSellerAdmin(admin.ModelAdmin):
         return form
 
     @admin.display(description="Изображение")
-    def product_image(self, obj):
+    def product_image(self, obj: ProductSeller) -> SafeString:
         """возвращает изображение товара если есть"""
         if obj.product.image:
             return mark_safe(f'<img src="{obj.product.image.url}" width="50" height="50">')
 
     @admin.display(description="Категория")
-    def product_category(self, obj):
+    def product_category(self, obj: ProductSeller) -> SafeString:
         """возвращает категорию для товара"""
         return obj.product.category
