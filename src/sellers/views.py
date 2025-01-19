46c23cdeed60db3e58a1164cdd13d6524d5d60c1
Yakovlev Sergey <monsterroot@gmail.com>
2025-01-20 01:45:01 +1000

    1. add annotations in sellers views 2. optimize duplicated code

--- src/sellers/views.py
+++ src/sellers/views.py
@@ -1,3 +1,4 @@
+from django.core.handlers.wsgi import WSGIRequest
 from django.http import JsonResponse
 from django.views import View
 from products.models import Product, Category
@@ -6,13 +7,11 @@ from products.models import Product, Category
 class ProductsForCategoryView(View):
     """Возвращает products по установленной category при смене категории"""
 
-    def get(self, request, category_id):
+    def get(self, request: WSGIRequest, category_id: int) -> JsonResponse:
         if category_id == 0:
-            products = Product.objects.all().values("id", "name")
+            products = Product.objects.all()
         else:
             category = Category.objects.get(id=category_id)
             descendants = category.get_descendants()
-            products = Product.objects.filter(category_id__in=[category_id] + [d.id for d in descendants]).values(
-                "id", "name"
-            )
-        return JsonResponse({"products": list(products)})
+            products = Product.objects.filter(category_id__in=[category_id] + [d.id for d in descendants])
+        return JsonResponse({"products": list(products.values("id", "name"))})
