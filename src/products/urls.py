7c1cdd36ef24397aa3ac0e9100e8ed5560a4330f
Petr Baikov <petr.bajjkov@rambler.ru>
2025-01-27 20:32:46 +0300

    Fix: product detail template

--- src/products/urls.py
+++ src/products/urls.py
@@ -12,6 +12,7 @@ from .views import (
 app_name = "products"
 
 urlpatterns = [
+    path('<int:pk>/',ProductDetailView.as_view(), name='product_detail'),
     path("product/<int:pk>/add_review/", add_review, name="add_review"),
     path("load_reviews/<int:pk>/<int:offset>/", load_reviews, name="load_reviews"),
     path("sellers/<int:pk>/", SellerDetailView.as_view(), name="seller_detail"),
