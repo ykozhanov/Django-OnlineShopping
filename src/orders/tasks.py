3c7850ef3c13367751847a84d04b38db4181a6eb
Petr Baikov <petr.bajjkov@rambler.ru>
2025-02-02 12:59:56 +0300

    Fix: fix doc in function

--- src/orders/tasks.py
+++ src/orders/tasks.py
@@ -7,7 +7,6 @@ def process_payment(order_pk, card_number, total_cost, api_url):
     This task sends a request to an external API to process a payment and returns the result
     of the operation
     """
-    print('process_payment start')
     payload = {
         'card_number': card_number,
         'order_id': order_pk,
@@ -15,7 +14,6 @@ def process_payment(order_pk, card_number, total_cost, api_url):
     }
     response = requests.post(api_url, data=payload)
     result = response.json()
-    print('process_payment end')
     if response.status_code == 200:
         return {'result': 'success', 'order_data':result}
 
--- src/paymentapi/services.py
+++ src/paymentapi/services.py
@@ -2,7 +2,9 @@ import random
 
 def fake_payment_service(card_number):
     """
-    Simulates a payment service by validating a card number and returning a payment result
+    Simulates a payment service by validating a card number and returning a payment result.
+    If the card number is even and does not end with 0, the payment is confirmed.
+    If the card number is odd and ends with 0, the service generates a random payment error.
     """
     card_number = card_number.replace(' ','')
     if int(card_number) % 2 == 0 and not card_number.endswith('0'):
